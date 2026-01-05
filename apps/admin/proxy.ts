66b8af0d5374f749d96ab732811027e621ead138
shemaikuzwe <eshemaikuzwe@gmail.com>
2026-01-05 11:42:21 +0200

    fix admin build

--- apps/admin/proxy.ts
+++ apps/admin/proxy.ts
@@ -17,6 +17,5 @@ export const config = {
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
     "/(api|trpc)(.*)",
-  ],
-  runtime:"nodejs"
+  ]
 };
